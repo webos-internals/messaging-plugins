e11b01846efc2dd9249279e63a287169d6f0a18f
unknown <Greg@.(none)>
2011-03-22 09:37:43 +1100

    3.0.5 work in progress...

--- 3.0/MessagingPlugins-Dist/package.sh
+++ 3.0/MessagingPlugins-Dist/package.sh
@@ -1,38 +1,5 @@
-if [ "$1" = "" ]
-then
-	exit 1
-fi
-
-#Get Commandline
-BuildType=$1
-
-#Get Compiler
-if [ "$BuildType" = "prephone" ]
-then
-	BuildCompiler="cs08q1armel"
-else
-	BuildCompiler="i686g25"
-fi
-
 # Package Plugins
 echo ""
 echo "Packing Messaging Plugins..."
-if [ "$BuildType" = "prephone" ]
-then
-{
-	#Compile Pixi
-	cp control.armv6 prephone/CONTROL/control
-	sh /root/optware/i686g25/staging/bin/ipkg-build $BuildType
-	
-	#Compile Pre
-	cp control.armv7 prephone/CONTROL/control
-	sh /root/optware/i686g25/staging/bin/ipkg-build $BuildType
-	
-	#Cleanup
-	rm -f prephone/CONTROL/control
-}
-else
-{
-	sh /root/optware/i686g25/staging/bin/ipkg-build $BuildType
-}
-fi
+sh /root/optware/i686g25/staging/bin/ipkg-build prephone
+sh /root/optware/i686g25/staging/bin/ipkg-build preemulator
